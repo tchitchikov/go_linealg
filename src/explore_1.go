d9182dea410f553d9ece155fcbf08bd61acbb87f
tchitchikov <spacerace.dev@gmail.com>
2016-09-23 09:23:42 -0400

    single threaded matrix creation

---
+++ src/explore_1.go
@@ -0,0 +1,41 @@
+/*
+   build a matrix and perform an operation on it.
+   give a row size, a column size, and an initialization
+*/
+
+package main
+
+import "fmt"
+
+// Matrix is a matrix initialization it has a row and column size and an initialization
+type Matrix struct {
+	iSize, jSize   int
+	initialization []int
+}
+
+func main() {
+	initializer := []int{1, 2, 3, 4, 5, 6}
+	matrix := BuildMatrix(Matrix{
+		iSize:          2,
+		jSize:          3,
+		initialization: initializer,
+	})
+	fmt.Println(matrix)
+}
+
+// BuildMatrix builds a matrix of a given size with a slice of numbers
+func BuildMatrix(m Matrix) [][]int {
+	var output [][]int
+	output = make([][]int, m.iSize)
+	for i := 0; i < m.iSize; i++ {
+		innerArray := make([]int, m.jSize)
+		if len(m.initialization) > 0 {
+			for position, val := range m.initialization[:m.jSize] {
+				innerArray[position] = val
+			}
+			m.initialization = m.initialization[m.jSize:]
+		}
+		output[i] = innerArray
+	}
+	return output
+}
