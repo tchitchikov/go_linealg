376345a8b431ad05d405e86c9ae39849926ee90e
tchitchikov <spacerace.dev@gmail.com>
2016-09-23 09:50:49 -0400

    added tests for initialized matricies

--- src/matrix_creation_test.go
+++ src/matrix_creation_test.go
@@ -14,3 +14,16 @@ func TestMatrixCreation(t *testing.T) {
 		t.Errorf("Expected a matrix of length 2, but it was %d instead.", len(matrix))
 	}
 }
+
+func TestMatrixCreationInitialized(t *testing.T) {
+	t.Log("Testing matrix_creation...")
+	initializer := []int{1, 2, 3, 4, 5, 6}
+	matrix := BuildMatrix(Matrix{
+		iSize:          2,
+		jSize:          3,
+		initialization: initializer,
+	})
+	if len(matrix) < 2 {
+		t.Errorf("Expected a matrix of length 2, but it was %d instead.", len(matrix))
+	}
+}
