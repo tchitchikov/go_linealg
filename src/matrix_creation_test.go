9b67fa3ab35c3df5b8b8b45a13fb349acea0c330
tchitchikov <spacerace.dev@gmail.com>
2016-09-23 09:59:16 -0400

    added stress tests

--- src/matrix_creation_test.go
+++ src/matrix_creation_test.go
@@ -27,3 +27,16 @@ func TestMatrixCreationInitialized(t *testing.T) {
 		t.Errorf("Expected a matrix of length 2, but it was %d instead.", len(matrix))
 	}
 }
+
+// TestMatrixCreationLarge makes a 1 million row by 1000 column matrix ~2.1 seconds on a desktop
+// Larger ones (1 billion took 71 seconds) overran the stack
+func TestMatrixCreationLarge(t *testing.T) {
+	t.Log("Testing matrix_creation...")
+	matrix := BuildMatrix(Matrix{
+		iSize: 1000000,
+		jSize: 1000,
+	})
+	if len(matrix) < 2 {
+		t.Errorf("Expected a matrix of length 2, but it was %d instead.", len(matrix))
+	}
+}
